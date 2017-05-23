3cd1ef9ddfba93d646b7d0ee4d1e197b04511111
moskovets <moskovets2009@yandex.ru>
2017-05-23 22:39:56 +0300

    add deleting of overlapping segments

--- laba9/algo.cpp
+++ laba9/algo.cpp
@@ -6,12 +6,26 @@
 #define SIGN(x) ((int) (x > 0) - (x < 0))
 
 struct tSegment {
+    bool empty = false;
     tPoint p1;
     tPoint p2;
     tSegment() {}
-    tSegment(tPoint a, tPoint b) {
-        p1 = a;
-        p2 = b;
+    tSegment(tPoint &a, tPoint &b) {
+        if(a.x == b.x && a.y > b.y) {
+            p2 = a;
+            p1 = b;
+        }
+        else {
+            p1 = a;
+            p2 = b;
+        }
+    }
+    bool isEqual(tSegment s) {
+        if(p1.isEqual(s.p1) && p2.isEqual(s.p2))
+            return true;
+        if(p1.isEqual(s.p2) && p2.isEqual(s.p1))
+            return true;
+        return false;
     }
 };
 struct tVector {
@@ -29,6 +43,11 @@ struct tVector {
         y = end.y - start.y;
         z = 0;
     }
+    tVector(tSegment s) {
+        x = s.p2.x - s.p1.x;
+        y = s.p2.y - s.p1.y;
+        z = 0;
+    }
 };
 
 void VectorMult(tVector &a, tVector& b, tVector &res) {
@@ -39,7 +58,15 @@ void VectorMult(tVector &a, tVector& b, tVector &res) {
 int ScalarMult(tVector &a, tVector& b) {
     return a.x * b.x + a.y * b.y + a.z * b.z;
 }
+bool CheckParal(tVector &a, tVector& b) {
+    double aa = sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
+    double bb = sqrt(b.x * b.x + b.y * b.y + b.z * b.z);
+    double sk = ScalarMult(a, b);
+    double angle = sk / aa / bb;
+    qDebug() << angle;
 
+    return fabs(angle) < 0.05;
+}
 int IsConvexPolygon(vector<tPoint> &polynom) {
     tVector a(polynom[1], polynom[0]);
     tVector b;
@@ -150,12 +177,130 @@ void PrintResPolynom(paintScene *scene, vector<tSegment> &segments, const QColor
         scene->addMyLine(segments[i].p1,segments[i].p2, colorBrush, 3);
     }
 }
-void GetSegmentsFromVertex(vector<tSegment> &segments, vector<tPoint> &resPolynom) {
+void GetSegmentsFromVertex(vector<tSegment> &segments, vector<tPoint> &resPolynom)
+{
     for(int i = 0; i < resPolynom.size() - 1; i++) {
         if(!resPolynom[i].isEqual(resPolynom[i+1])) {
-            segments.push_back(tSegment(resPolynom[i],resPolynom[i+1]));
+            if(resPolynom[i].x <= resPolynom[i + 1].x) {
+                segments.push_back(tSegment(resPolynom[i],resPolynom[i+1]));
+            }
+            else {
+                segments.push_back(tSegment(resPolynom[i+1],resPolynom[i]));
+            }
         }
     }
+}
+void DeleteDoubleAndNullSegments(vector<tSegment> &segments)
+{
+    vector<tSegment> resSegment;
+    for(int i = 0; i < segments.size(); i++) {
+        bool flag = true;
+        if(segments[i].empty)
+            continue;
+        for(int j = 0; j < resSegment.size(); j++) {
+            if(segments[i].isEqual(resSegment[j])) {
+                flag = false;
+                break;
+            }
+        }
+        if(flag)
+            resSegment.push_back(segments[i]);
+    }
+    segments = resSegment;
+}
+bool IsSegmentsOnLineAndHaveIntersection(tSegment &s1, tSegment &s2) {
+    tVector v1(s1);
+    tVector v2(s2);
+    tVector h(v1.y, -v1.x);
+    if(CheckParal(h, v2)) {
+        tVector v3(s1.p1, s2.p1);
+        qDebug() << "hey";
+        if(CheckParal(h, v3)) {
+            qDebug() << "lala";
+            if(s1.p1.x == s1.p2.x) { //vertical segments
+                if(s1.p1.y <= s2.p2.y && s1.p1.y >= s2.p1.y)
+                    return true;
+                if(s1.p2.y <= s2.p2.y && s1.p2.y >= s2.p1.y)
+                    return true;
+            }
+            else {
+                if(s1.p1.x <= s2.p2.x && s1.p1.x >= s2.p1.x)
+                    return true;
+                if(s1.p2.x <= s2.p2.x && s1.p2.x >= s2.p1.x)
+                    return true;
+            }
+        }
+    }
+    return false;
+}
+void AddToListSegmentsResultsOfIntersection(vector<tSegment> &segments, tSegment &s1, tSegment &s2) {
+    if(s1.p1.x == s2.p2.x || s2.p1.x == s1.p2.x)
+        return;
+    s1.empty = true;
+    s2.empty = true;
+    if(s1.p1.isEqual(s2.p1) &&  s1.p2.isEqual(s2.p2))
+        return;
+    bool flagchange = false;
+    if(s1.p1.x == s1.p1.x) {
+        if(s2.p1.y < s1.p1.y) {
+            segments.push_back(tSegment(s2.p1, s1.p1));
+        }
+        else {
+            segments.push_back(tSegment(s1.p1, s2.p1));
+        }
+        tPoint t, end;
+        if(s2.p2.y <= s1.p2.y) {
+            t = s2.p2;
+            end = s1.p2;
+        }
+        else {
+            t = s1.p2;
+            end = s2.p2;
+        }
+        flagchange = true; //TODO;
+         segments.push_back(tSegment(t, end));
+         return;
+    }
+
+    if(s2.p1.x < s1.p1.x) {
+        segments.push_back(tSegment(s2.p1, s1.p1));
+    }
+    else {
+        segments.push_back(tSegment(s1.p1, s2.p1));
+    }
+    tPoint t, end;
+    if(s2.p2.x <= s1.p2.x) {
+        t = s2.p2;
+        end = s1.p2;
+    }
+    else {
+        t = s1.p2;
+        end = s2.p2;
+    }
+    //segments.push_back(tSegment(s1.p1, t)); //del
+    //segments.push_back(tSegment(s1.p1, t)); //del
+    segments.push_back(tSegment(t, end));
+}
+
+void FindOverlappingSegments(vector<tSegment> &segments)
+{
+   // vector<tSegment> resSegment;
+    for(int i = 0; i < segments.size(); i++) {
+        if(!segments[i].empty) {
+            for(int j = 0; j < segments.size(); j++) {
+                //qDebug() << "hop";
+                if(segments[j].empty || i == j)
+                    continue;
+                if(IsSegmentsOnLineAndHaveIntersection(segments[i], segments[j])) {
+                    qDebug() << "ok";
+                    AddToListSegmentsResultsOfIntersection(segments, segments[i], segments[j]);
+                }
+            }
+        }
+    }
+  //  segments = resSegment;
+
+    DeleteDoubleAndNullSegments(segments);
 }
 
 int SimpleAlgo(paintScene *scene, const QColor &colorBrush)
@@ -169,6 +314,7 @@ int SimpleAlgo(paintScene *scene, const QColor &colorBrush)
     CutSegment(scene->polynom, normVect, scene->polyForCut, resPolynom, scene);
     vector<tSegment> segments;
     GetSegmentsFromVertex(segments, resPolynom);
+    FindOverlappingSegments(segments);
 
     PrintResPolynom(scene, segments, colorBrush);
 
